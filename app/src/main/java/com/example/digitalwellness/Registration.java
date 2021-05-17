22247f0110417d0dc22f110c5c094abb8dc70180
tiewweijian <twj1998@gmail.com>
2021-05-17 20:55:02 +0800

    Yo just updated a few stuffss

--- app/src/main/java/com/example/digitalwellness/Registration.java
+++ app/src/main/java/com/example/digitalwellness/Registration.java
@@ -1,10 +1,21 @@
 package com.example.digitalwellness;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registration extends AppCompatActivity {
 
@@ -16,7 +27,41 @@ public class Registration extends AppCompatActivity {
         setContentView(R.layout.activity_registration2);
         mAuth = FirebaseAuth.getInstance();
 
+        EditText editEmail = (EditText) findViewById(R.id.regEmail);
 
+        EditText editPassword = (EditText) findViewById(R.id.regPassword);
 
+        final Button regButton = findViewById(R.id.regButton);
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String regEmail = editEmail.getText().toString();
+                String regPassword = editPassword.getText().toString();
+                createAccount(regEmail, regPassword);
+            }
+        });
     }
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Success", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(Registration.this, "User created",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Failure", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Registration.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
