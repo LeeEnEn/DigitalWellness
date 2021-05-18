a37b0bcfd19835aae3a392a2cb8eabc57329353f
tiewweijian <twj1998@gmail.com>
2021-05-18 22:20:54 +0800

    yo i tried :/

--- app/src/main/java/com/example/digitalwellness/MainActivity.java
+++ app/src/main/java/com/example/digitalwellness/MainActivity.java
@@ -7,8 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
-    // HI this change is made by wei jian.
+
+    //For Facebook Registration
+    private static final String EMAIL = "EMAIL";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
         final Button regButton = findViewById(R.id.mainRegister);
         final Button loginButton = findViewById(R.id.mainLogin);
-
+        final Button facebookButton = findViewById(R.id.facebookreg);
+        final Button googleButton = findViewById(R.id.googlereg);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
