5eda7adc6b2f0295bb595c007064d1cc1b7de6fa
tiewweijian <twj1998@gmail.com>
2021-06-07 01:24:12 +0800

    Changes (7 June) - Minor UI changes for Screen Tracker

--- app/src/main/java/com/example/digitalwellness/ScreenTimeTracker.java
+++ app/src/main/java/com/example/digitalwellness/ScreenTimeTracker.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
@@ -43,12 +45,29 @@ public class ScreenTimeTracker extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
         switch(item.getItemId()) {
             case android.R.id.home:
                 this.finish();
                 return true;
         }
+
+        if (id == R.id.setting) {
+            startActivity(new Intent(ScreenTimeTracker.this, Settings.class));
+        } else if (id == R.id.options_logout) {
+            firebaseHelper.logoutUser();
+            startActivity(new Intent(ScreenTimeTracker.this, StartMenu.class));
+
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate menu
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/digitalwellness/Settings.java
+++ app/src/main/java/com/example/digitalwellness/Settings.java
@@ -3,8 +3,10 @@ package com.example.digitalwellness;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.preference.PreferenceActivity;
+import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 
 public class Settings extends PreferenceActivity {
 
@@ -13,7 +15,5 @@ public class Settings extends PreferenceActivity {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.prefs);
-
-
     }
 }
